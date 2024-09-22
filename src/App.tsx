25fd219d9c41b6e6dc9649ef2f1fc3bbd17ee501
Abhishek Sharma <sharmabhishek802158@gmail.com>
2024-09-22 17:30:01 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BorderBeam } from "./components/magicui/border-beam.tsx";
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="relative h-[200px] w-[200px] rounded-xl bg-slate-600">
+      <p>Hello</p>
+    <BorderBeam />
+    <p>World</p>
+  </div>
+  )
+}
+
+export default App
